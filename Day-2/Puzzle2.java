729647dfc1f1f6676f0fc14c4b12d28221a6d9cc
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-03-28 23:31:13 +0530

    Add Day 2

---
+++ Day-2/Puzzle2.java
@@ -0,0 +1,62 @@
+package com.prog.AdventOfCode2018;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Puzzle2 {
+
+	static final int NUMBER_OF_CHARS = 256;
+	public static void main(String[] args) {
+		
+		
+		int checksum = findCheckSum();
+		System.out.println("Checksum is: "+checksum);
+		
+	}
+	private static int findCheckSum() {
+		
+		int count[] = new int[NUMBER_OF_CHARS];
+		int countTwos = 0, countThrees = 0;
+		boolean twoOnce = false, threeOnce = false;
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader("D:\\AshmiD\\ASHMI\\GitHub\\advent-of-code-2018\\Day-2\\input.txt"));
+			String line = reader.readLine();
+			
+			while(line != null) {
+				twoOnce = false; threeOnce = false;
+				count = new int[NUMBER_OF_CHARS];
+				getFrequencyOfChars(line, count);
+				for(int i = 0; i<NUMBER_OF_CHARS; i++) {
+						
+					if(count[i] == 2 && twoOnce != true) {
+						twoOnce = true;
+						countTwos++;
+					}
+						
+					else if(count[i] == 3 && threeOnce != true){
+						threeOnce = true;
+						countThrees++;
+					}
+						
+
+				}
+				line = reader.readLine();
+			}			 
+		}
+		catch(IOException e) {
+			
+		}
+		
+		return (countThrees*countTwos);
+	}
+	private static void getFrequencyOfChars(String line, int[] count) {
+		
+		for(int i = 0; i<line.length(); i++) {
+			count[line.charAt(i)]++;
+		}
+		
+	}
+
+}
---
+++ Day-2/Puzzle2_Part2.java
@@ -0,0 +1,66 @@
+package com.prog.AdventOfCode2018;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Puzzle2_Part2 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		// Compare 2 words letter by letter
+		// fileContent reads the entire file into a String 
+		@SuppressWarnings("resource")
+		String fileContent = new Scanner(new File("D:\\AshmiD\\ASHMI\\GitHub\\advent-of-code-2018\\Day-2\\input.txt"))
+				.useDelimiter("\\Z").next();
+		
+		String commonString = getCommonString(fileContent);
+		System.out.println("IDs differing by 1 character is: "+commonString);
+
+	}
+
+	private static String getCommonString(String fileContent) {
+
+		String words[] = fileContent.split("[\\r\\n]+");
+		String word1, word2, same;
+		int difference;
+		
+		for(int i = 0; i< words.length-1; i++) {
+			for(int j = i+1; j< words.length; j++) {
+				
+				word1 = words[i];
+				word2 = words[j];
+				difference = 0;
+				same = "";
+				
+				for(int k = 0; k < word1.length(); k++) {
+					if(word1.charAt(k) != word2.charAt(k))
+						difference++;
+					else
+						same += word1.charAt(k);
+					if(difference > 1)
+						break;
+				}
+				if(difference == 1)
+					return same;
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		return null;
+	}
+
+}
